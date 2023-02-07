f37c537b6e7ab704a1d45256f493d6c25f88c074
Kishan Bhimani <kbhimani@architech.ca>
2023-02-07 16:43:41 -0500

    00004 solution added

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,4 @@
-type MyPick<T, K> = any
+//https://stackoverflow.com/questions/49397567/how-to-remove-properties-via-mapped-type-in-typescript
+type MyPick<T, K> = {
+  [P in keyof T as P extends K ? P : never]: T[P]
+}
